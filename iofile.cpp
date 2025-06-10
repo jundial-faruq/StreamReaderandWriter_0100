a877d3880e267bc8b8a5bf5346507cb137bed6f4
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-06-10 14:55:21 +0700

    jika file ada maka

--- iofile.cpp
+++ iofile.cpp
@@ -34,6 +34,12 @@ int main() {
     // menunjuk ke sebuah file
     infile.open("contohfile.txt");
     cout << endl << ">= Membuka dan Membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        
+    }
+    
 
     
     return 0;
