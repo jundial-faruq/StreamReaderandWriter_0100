20e15111b7ddbc44f240efc78d80433079e82c7f
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-06-10 15:08:51 +0700

    memulai dengan array  int 3 elemen

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -8,6 +8,12 @@ using namespace std;
 
 int main() {
     cout << "Awal program" << endl; // penanda 1:...
+    try{
+        array<int, 3> data = {10, 20, 30};
+        //pesan array interger 3 elemen
+        cout <<data.at(5) << endl;
+        
+    }
 }
 
 
