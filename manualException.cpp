6248a319e167c0f61abe028546871244ab27dc77
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-06-10 15:20:46 +0700

    jika berbentuk selain int maka ini yang dieksekusi

--- manualException.cpp
+++ manualException.cpp
@@ -11,5 +11,11 @@ int main() {
         // stok ini akan dieksekusi
         cout << "Pengecualian akan dieksekusi" << endl;
     }
+    catch (...) {
+        /*Jika selain integer maka block ini akan dieksekusi*/
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
     
 }
